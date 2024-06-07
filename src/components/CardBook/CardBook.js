a0fa8ef29642d2284118e3ea823f36661200845b
Thaymelva <thaymelva@gmail.com>
2024-06-06 23:01:54 -0300

    api

---
+++ src/components/CardBook/CardBook.js
@@ -0,0 +1,26 @@
+import styles from './CardBook.module.css';
+
+function CardBook({id, livro, autor, categoria, handlerRemove}) {
+
+    const remove = (event)=>{
+        event.preventDefault();
+        handlerRemove(id);
+    }
+
+    return(
+        <div className={styles.book_card}>
+            <h4>{livro}</h4>
+            <p>{autor}</p>
+            <p className={styles.category_text}>
+                <span></span>{categoria}
+            </p>
+            <div className={styles.book_card_actions}>
+                <button onClick={remove}>
+                    Excluir
+                </button>
+            </div>
+        </div>
+    )
+}
+
+export default CardBook;
